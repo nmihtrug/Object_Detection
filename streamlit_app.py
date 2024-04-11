72179182a829ce51d2e55f9f7bad632634808fab
TrungAzieL <trung.nguyenminh7804@gmail.com>
2024-04-11 22:50:53 +0700

    fix_deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
-from object_detection import object_detection
 import os
 from datetime import datetime
+from ultralytics import YOLO
+import tempfile
+from glob import glob
 
 
 def config_container():
@@ -9,49 +11,49 @@ def config_container():
         "Choose a model", ["yolov8n", "yolov8m", "yolov8l", "yolov8x"]
     )
     file_upload = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-    
+
     if file_upload is not None:
         image = file_upload.read()
         st.image(image, caption="Uploaded Image")
         st.write("")
-    
+
     return model_choosens, file_upload
 
 
 def prediction_container(model_choosens, file_upload):
     if file_upload is not None:
+        st.write("Detecting...")
+        for model_choosen in model_choosens:
+            model = YOLO(f"model/{model_choosen}.pt")
 
-        for model in model_choosens:
-            saved_image_path = os.path.join(
-                "data",
-                file_upload.name.split(".")[0]
-                + datetime.now().strftime("%d%m%Y_%H%M%S")
-                + f"_{model}."
-                + file_upload.name.split(".")[1],
-            )
+            temp_dir = tempfile.TemporaryDirectory()
 
-            with open(saved_image_path, "wb") as f:
-                f.write(file_upload.getbuffer())
+            uploaded_file_name = file_upload.name
+            uploaded_file_path = os.path.join(temp_dir.name, uploaded_file_name)
 
-            st.write("Detecting...")
-            results = object_detection(saved_image_path, model)
+            with open(uploaded_file_path, "wb") as f0:
+                f0.write(file_upload.getbuffer())
 
-            for subfolder in os.listdir("pred"):
-                for file in os.listdir(os.path.join("pred", subfolder)):
-                    if file == saved_image_path.split("/")[-1]:
-                        st.image(
-                            os.path.join("pred", subfolder, file),
-                            caption=f"Prediction using {model} model",
-                            use_column_width=True,
-                        )
-                        break
+            results = model.predict(
+                source=uploaded_file_path,
+                project=temp_dir.name,
+                name="result",
+                save=True,
+                exist_ok=True,
+            )  # save predictions as label
+            for f in glob(os.path.join(temp_dir.name, "result", "*")):
+                st.image(
+                    f,
+                    caption=f"Prediction using {model_choosen} model",
+                    use_column_width=True,
+                )
 
 
 def main():
     st.set_page_config(layout="wide")
     st.title("Object Detection")
 
-    config, prediction = st.columns([4, 5], gap='large')
+    config, prediction = st.columns([4, 5], gap="large")
 
     with config:
         file_upload, model_choosens = config_container()
